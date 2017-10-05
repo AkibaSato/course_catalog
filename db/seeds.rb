c0cdb5df24bd026b97d9257ca3499362a3b50dd0
Akiba Sato <akibasato1992@gmail.com>
2017-10-04 22:56:25 -0400

    Added databases and basic models.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+
+file = File.read('instructor.json')
+data_hash = JSON.parse(file)
+
+data_hash.each do |instructor|
+  Instructor.create(first: instructor['first'],
+                    middle: instructor['middle'],
+                    last: instructor['last'],
+                    email: instructor['email'])
+end
+
+# Course parsing
+file = File.read('course.json')
+data_hash = JSON.parse(file)
+
+data_hash.each do |course|
+  Course.create(name: course['name'],
+                number: course['id'])
+  subjects = course['subjects']
+end
+
+# Subject parsing
+file = File.read('subject.json')
+data_hash = JSON.parse(file)
+
+data_hash.each do |subject|
+  Subject.create(name: subject['name'],
+                term: subject['term'],
+                abbreviation: subject['abbreviation'])
+end
