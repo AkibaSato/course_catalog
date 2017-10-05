806d79e8742d2004a5404fac71cfc0a0909816aa
Akiba Sato <akibasato1992@gmail.com>
2017-10-05 00:01:34 -0400

    Added homepage and Bootstrap toolbar.

--- Gemfile
+++ Gemfile
@@ -14,6 +14,9 @@ end
 gem 'rails', '~> 5.1.4'
 # Use sqlite3 as the database for Active Record
 #gem 'sqlite3'
+gem 'jquery-rails'
+
+gem 'bootstrap-sass', '~> 3.3.6'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   resources :instructors
   resources :subjects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'welcome#home'
+
+  get 'instructor', to: 'instructors#new'
+  get 'course', to: 'courses#new'
+  get 'subject', to: 'subjects#new'
+
+
 
 end
