9be3ccf62ad6a6d97181202b5efec3411509a4b2
Akiba Sato <akibasato1992@gmail.com>
2017-10-05 00:16:07 -0400

    Edited user migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171005040352) do
+ActiveRecord::Schema.define(version: 20171005041033) do
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20171005040352) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
