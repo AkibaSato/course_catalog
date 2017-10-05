c2545509d01fd5499afa4e1e99b8425cdd05f237
Akiba Sato <akibasato1992@gmail.com>
2017-10-05 00:06:09 -0400

    Added users database.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
+  resources :users
   resources :courses
   resources :instructors
   resources :subjects
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#home'
 
-  get 'instructor', to: 'instructors#new'
-  get 'course', to: 'courses#new'
-  get 'subject', to: 'subjects#new'
+  get 'instructor', to: 'instructors#index'
+  get 'course', to: 'courses#index'
+  get 'subject', to: 'subjects#index'
+  get 'user', to: 'user#index'
+
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171005021302) do
+ActiveRecord::Schema.define(version: 20171005040352) do
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 20171005021302) do
     t.string "name"
     t.string "term"
     t.string "abbreviation"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
