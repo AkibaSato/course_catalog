00e7051ad3328af0c5bac0acb31a0f80e977a30d
Akiba Sato <akibasato1992@gmail.com>
2017-11-07 12:53:54 -0500

    Modified search function.

--- app/models/subject_courses_relation.rb
+++ app/models/subject_courses_relation.rb
@@ -1,2 +1,7 @@
 class SubjectCoursesRelation < ApplicationRecord
+  belongs_to :subject, :class_name => 'Subject', foreign_key: 'subject_id'
+  belongs_to :course, :class_name => 'Course', foreign_key: 'course_id'
+
+  # belongs_to :subject, :class_name => 'Subject', primary_key: 'university_id', foreign_key: 'subject_id'
+  # belongs_to :course, :class_name => 'Course', primary_key: 'code', foreign_key: 'course_id'
 end
